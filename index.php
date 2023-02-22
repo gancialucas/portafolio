66a1f9f2dadafe0a90b0d456ba36cc18a66f4c3a
gancialucas <lucasgancia@gmail.com>
2023-02-22 17:22:09 -0300

    containers out

--- index.php
+++ index.php
@@ -7,32 +7,30 @@
 ?>
 
         <section class="home">
-            <div class="container">
                 <div class="text-title">Bienvenido a tu portafolio</div>
 
                 <?php foreach ($proyectos as $proyecto) {?>
-                        <div class="card text-start">
-                            <div class="card">
-                                <div class="card-header">
-                                    #<?php echo $proyecto['id']; ?>. no sé
-                                </div>
+                    <div class="card text-start">
+                        <div class="card">
+                            <div class="card-header">
+                                #<?php echo $proyecto['id']; ?>. no sé
+                            </div>
 
-                                <div class="card-body">
-                                    <div class="row align-items-center">
-                                        <div class="col-6">
-                                            <img width="100px" class="img-fluid mx-auto d-block" src="assets/img/<?php echo $proyecto['archivo']; ?>" alt="imagen_proyecto">
-                                        </div>   
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-6">
+                                        <img width="100px" class="img-fluid mx-auto d-block" src="assets/img/<?php echo $proyecto['archivo']; ?>" alt="imagen_proyecto">
+                                    </div>   
 
-                                        <div class="col-6">
-                                            <h1 class="card-title"><?php echo $proyecto['nombre']; ?></h1>
-                                            <p class="card-text"><?php echo $proyecto['descripcion']; ?></p>
-                                        </div>
+                                    <div class="col-6">
+                                        <h1 class="card-title"><?php echo $proyecto['nombre']; ?></h1>
+                                        <p class="card-text"><?php echo $proyecto['descripcion']; ?></p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <?php }?>
-            </div><br>      
+                    </div><br>
+                <?php }?>  
         </section>
 
 <?php include("footer.php"); ?>
--- portafolio.php
+++ portafolio.php
@@ -36,63 +36,61 @@
         $proyectos = $obj_conexion->consultar("SELECT * FROM `proyectos`"); 
 ?>
 
-        <section class="home">
-            <div class="container">
-                <div class="text-title">#. Sube tu proyecto</div>
+        <section class="home">            
+            <div class="text-title">#. Sube tu proyecto</div>
+                <div class="card text-start">
+                    <div class="card">
+                        <div class="card-header">
+                            #no sé - Datos del proyecto
+                        </div>
+
+                        <div class="card-body">
+                            <form action="portafolio.php" method="post" enctype="multipart/form-data">
+                                <div class="form-outline m-2">
+                                        <input required type="file" class="form-control" name="archivo">
+                                </div>        
+                                <div class="form-outline m-2">
+                                        <input required type="text" class="form-control" name="nombre" placeholder="Nombre">
+                                </div>
+                                <div class="form-outline m-2">
+                                        <input required type="text" class="form-control" name="descripcion">
+                                </div>
+                                <div class="form-outline m-2">
+                                        <input type="submit" class="btn btn-secondary" value="Enviar proyecto">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>    
+                
+                <!-- MOSTRAR Y EDITAR PROYECTOS SUBIDOS -->
+                <br><div class="text-title">#. Crud</div>
+
+                <?php foreach ($proyectos as $proyecto) {?>
                     <div class="card text-start">
                         <div class="card">
                             <div class="card-header">
-                                #no sé - Datos del proyecto
+                                #<?php echo $proyecto['id']; ?>. no sé
                             </div>
 
                             <div class="card-body">
-                                <form action="portafolio.php" method="post" enctype="multipart/form-data">
-                                    <div class="form-outline m-2">
-                                            <input required type="file" class="form-control" name="archivo">
-                                    </div>        
-                                    <div class="form-outline m-2">
-                                            <input required type="text" class="form-control" name="nombre" placeholder="Nombre">
-                                    </div>
-                                    <div class="form-outline m-2">
-                                            <input required type="text" class="form-control" name="descripcion">
-                                    </div>
-                                    <div class="form-outline m-2">
-                                            <input type="submit" class="btn btn-secondary" value="Enviar proyecto">
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>    
-                    
-                    <!-- MOSTRAR Y EDITAR PROYECTOS SUBIDOS -->
-                    <br><div class="text-title">#. Crud</div>
+                                <div class="row align-items-center">
+                                    <div class="col-6">
+                                        <img width="100px" class="img-fluid mx-auto d-block" src="assets/img/<?php echo $proyecto['archivo']; ?>" alt="imagen_proyecto">
+                                    </div>   
 
-                    <?php foreach ($proyectos as $proyecto) {?>
-                        <div class="card text-start">
-                            <div class="card">
-                                <div class="card-header">
-                                    #<?php echo $proyecto['id']; ?>. no sé
-                                </div>
-
-                                <div class="card-body">
-                                    <div class="row align-items-center">
-                                        <div class="col-6">
-                                            <img width="100px" class="img-fluid mx-auto d-block" src="assets/img/<?php echo $proyecto['archivo']; ?>" alt="imagen_proyecto">
-                                        </div>   
-
-                                        <div class="col-6">
-                                            <h1 class="card-title"><?php echo $proyecto['nombre']; ?></h1>
-                                            <p class="card-text"><?php echo $proyecto['descripcion']; ?></p>
-                                        </div>
+                                    <div class="col-6">
+                                        <h1 class="card-title"><?php echo $proyecto['nombre']; ?></h1>
+                                        <p class="card-text"><?php echo $proyecto['descripcion']; ?></p>
                                     </div>
-                                </div>
-                                <div class="card-footer">
-                                    <a class="btn btn-danger" href="?borrar=<?php echo $proyecto['id']; ?>">Eliminar</a>
                                 </div>
                             </div>
+                            <div class="card-footer">
+                                <a class="btn btn-danger" href="?borrar=<?php echo $proyecto['id']; ?>">Eliminar</a>
+                            </div>
                         </div>
-                    <?php }?>
-            </div><br>      
+                    </div><br> 
+                <?php }?>
         </section>
 
 <?php include("footer.php");?>
